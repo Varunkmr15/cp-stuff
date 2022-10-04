0c8b7d911d646f39e70de3e73899b9d12b51a8da
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2022-10-04 11:51:45 +0530

    Create EulerTotientFunction.cpp

---
+++ Number Theory/EulerTotientFunction.cpp
@@ -0,0 +1,43 @@
+int phi(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+//Euler totient function from 1 to n in O(nloglogn) 
+
+void phi_1_to_n(int n) {
+    vector<int> phi(n + 1);
+    for (int i = 0; i <= n; i++)
+        phi[i] = i;
+
+    for (int i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j <= n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}
+
+
+//Finding the totient from 1 to n using the divisor sum property O(nlogn)
+void phi_1_to_n(int n) {
+    vector<int> phi(n + 1);
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i - 1;
+
+    for (int i = 2; i <= n; i++)
+        for (int j = 2 * i; j <= n; j += i)
+              phi[j] -= phi[i];
+}
+
