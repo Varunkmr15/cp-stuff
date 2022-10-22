1f99e323dcae49e99f815099183396f9e13edeab
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2022-10-22 13:32:06 +0530

    Update EulerTotientFunction1ToN.cpp

--- Number Theory/EulerTotientFunction1ToN.cpp
+++ Number Theory/EulerTotientFunction1ToN.cpp
@@ -1,3 +1,4 @@
+//Using sieve of eratosthenes
 // TC:= O(NloglogN)
 
 void phi_1_to_n(int n) {
@@ -11,4 +12,20 @@ void phi_1_to_n(int n) {
                 phi[j] -= phi[j] / i;
         }
     }
+}
+
+
+//Using divisor sun property
+// TC:= O(NlogN)
+
+void phi_1_to_n(int n) {
+    vector<int> phi(n + 1);
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i - 1;
+
+    for (int i = 2; i <= n; i++)
+        for (int j = 2 * i; j <= n; j += i)
+              phi[j] -= phi[i];
 }
