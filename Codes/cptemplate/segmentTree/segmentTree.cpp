299a383788fd5836d04202581a7bd03038a8f884
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2025-03-15 18:12:50 +0530

    Update segmentTree.cpp

--- Codes/cptemplate/segmentTree/segmentTree.cpp
+++ Codes/cptemplate/segmentTree/segmentTree.cpp
@@ -1,55 +1,116 @@
-int CST(int ss, int se, int si, vector<int>&arr, vector<int>&tree)
-{
-    if (ss == se)
-    {
-        tree[si] = arr[ss];
-        return arr[ss];
+// === Segment Tree ===
+struct Node;
+struct Update;
+
+template <typename T, typename Node = Node, typename Update = Update>
+class SegmentTree {
+private:
+    int size = 0;
+    vector<Node> seg;
+
+    void build (int start, int end, int ind, vector<T> &arr) {
+        if (start == end) {
+            seg[ind] = Node(arr[start]);
+            return;
+        }
+        int mid = (start + end) / 2;
+        int leftInd = 2 * ind + 1, rightInd = 2 * ind + 2;
+        build(start, mid, leftInd, arr);
+        build(mid + 1, end, rightInd, arr);
+        seg[ind].merge(seg[leftInd], seg[rightInd]);
+    }
+    // start = 0, end = n-1, ind = 0 , {left,right} -> input
+    Node query (int start, int end, int ind, int left, int right) {
+        if (start > right || end < left) {
+            return Node();
+        }
+        if (start >= left && end <= right) {
+            return seg[ind];
+        }
+        int mid = (start + end) / 2;
+        int leftInd = 2 * ind + 1, rightInd = 2 * ind + 2;
+        Node res;
+        Node leftItem = query(start, mid, leftInd, left, right);
+        Node rightItem = query(mid + 1, end, rightInd, left, right);
+        res.merge(leftItem, rightItem);
+        return res;
     }
 
-    int mid = (ss + se) / 2;
+    void update (int start, int end, int ind, int index, Update &u) {
+        if (start == end) {
+            u.apply(seg[ind]);
+            return;
+        }
+        int mid = (start + end) / 2;
+        int leftInd = 2 * ind + 1, rightInd = 2 * ind + 2;
+        if (index <= mid) update(start, mid, leftInd, index, u);
+        else update(mid + 1, end, rightInd, index, u);
+        seg[ind].merge(seg[leftInd], seg[rightInd]);
+    }
 
-    tree[si] = CST(ss, mid, 2 * si + 1, arr, tree)
-               + CST(mid + 1, se, 2 * si + 2, arr, tree);
-
-    return tree[si];
-}
-
-//initially ss = 0, se = n-1, {qs,qe} -> user range input, si = 0 (root)
-int getSegTreeRangeSum(int qs, int qe, int ss, int se, int si, vector<int> &tree)
-{
-    if (se < qs || ss > qe)
-        return 0;
-    if (qs <= ss &&  qe >= se)
-        return tree[si];
-
-    int mid = (ss + se) / 2;
-
-    return getSegTreeRangeSum(qs, qe, ss, mid, 2 * si + 1, tree)
-           + getSegTreeRangeSum(qs, qe, mid + 1, se, 2 * si + 2, tree);
-}
-
-//initially ss = 0, se = n-1, i -> update index, si = 0 (root)
-// calculate diff = new_value - tree[i]
-void updateSegTree(int ss, int se, int i, int si, int diff, vector<int> &tree)
-{
-    if (i < ss || i > se)
-        return;
-
-    tree[si] = tree[si] + diff;
-
-    if (se > ss)
-    {
-        int mid = (ss + se) / 2;
-
-        updateSegTree(ss, mid, i, 2 * si + 1, diff, tree);
-        updateSegTree(mid + 1, se, i, 2 * si + 2, diff, tree);
+public:
+    SegmentTree () {
 
     }
-}
 
-vector<int> segmentTree(vector<int>&arr) {
-    int n = arr.size();
-    vector<int>tree(4 * n);
-    CST(0, n - 1, 0, arr, tree);
-    return tree;
-}
+    SegmentTree (int n) {
+        size = n;
+        seg.resize(4 * size + 1);
+        vector<T> arr(n, 0);
+        build(arr);
+    }
+
+    SegmentTree (vector<T> &arr) {
+        size = arr.size();
+        seg.resize(4 * size + 1);
+        build(arr);
+    }
+
+    void build (vector<T> &arr) {
+        build(0, size - 1, 0, arr);
+    }
+    // {left,right} -> input
+    // Use this
+    Node query (int left, int right) {
+        return query(0, size - 1, 0, left, right);
+    }
+
+    void update (int index, long long value) {
+        Update u = Update(value);
+        return update(0, size - 1, 0, index, u);
+    }
+};
+
+struct Node {
+    long long sum, mx, mn;
+
+    Node () : sum(0), mx(-1e17), mn(1e17) {}
+    Node (long long val) : sum(val), mx(val), mn(val) {}
+
+    void merge (Node &left, Node &right) {
+        sum = left.sum + right.sum;
+        mx = max(left.mx, right.mx);
+        mn = min(left.mn, right.mn);
+    }
+};
+
+struct Update {
+    long long val;
+
+    Update () : val(0) {}
+    Update (long long v) : val(v) {}
+
+    void apply (Node &node) {
+        node.sum = val;
+        node.mn = val;
+        node.mx = val;
+    }
+};
+// Example : 
+// void solve() {
+//     vector<int>v = {10, 20, 30, 40};
+//     int n = sz(v);
+//     SegmentTree<int> st(v);
+//     st.update(2, 40);
+//     cout << st.query(1, 2).mx << nline;
+// }
